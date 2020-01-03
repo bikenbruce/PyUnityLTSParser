0ed346e640902ad1eb1ff769de3b5c7866d68a5f
Bruce Gardner <bruce.gardner@sjsu.edu>
2020-01-03 15:18:20 -0800

    added testing for local file version vrs remote / server verion. - not complete

--- read.py
+++ read.py
@@ -1,36 +1,67 @@
+import argparse
 from bs4 import BeautifulSoup
 import feedparser
-from os import path
+import os
 import requests
 
-def current_version(version):
-	if path.exists(version):
-		print('does not exist')
-	else:	
-		print('does exists')
+def local_version(debug, server_version):
+	local_version_file = 'downloads/local_version.txt'
+	# make directory if it does not exist
+	if not os.path.isdir('downloads'):
+		if debug: print('creating download directory')
+		os.makedirs('downloads')
 
-feed = feedparser.parse('https://unity3d.com/unity/lts-releases.xml')
+	# folder exists, but not file
+	if not os.path.exists(local_version_file):
+		if debug: print('creating local version file')
+		f = open(local_version_file, 'w')
+		f.write(server_version, "\n")
+		f.close()
+		# return
 
-summary = feed.entries[0]['summary_detail']['value']
-version = feed.entries[0]['title_detail']['value']
+	else:
+		# file and folder exists, open file, read content
+		f = open(local_version_file, 'r')
+		file_version = f.readline()
+		if not file_version in server_version:
+			if debug: print('local version does not match server version')
+		else:
+			if debug: print('local version does match server version')
+		f.close()
 
-print(version)
+		if debug: print('local:', file_version, 'server:', server_version)
 
-current_version(version)
+def check_state(debug): 
+	feed = feedparser.parse('https://unity3d.com/unity/lts-releases.xml')
 
-soup = BeautifulSoup(summary, 'lxml')
+	server_version = feed.entries[0]['title_detail']['value']
+	summary = feed.entries[0]['summary_detail']['value']
 
-tags = soup.find_all('a', href=True)
+	if debug: print('server version:', server_version)
+	
+	local_version(debug, server_version.strip())
 
-for tag in tags:
-	url = tag.get('href')
-	if '.pkg' in url:
-		file_name = url.rsplit('/', 1)[1]
-		# print(file_name)
+	soup = BeautifulSoup(summary, 'lxml')
+
+	tags = soup.find_all('a', href=True)
+
+	for tag in tags:
+		url = tag.get('href')
+		if '.pkg' in url:
+			file_name = url.rsplit('/', 1)[1]
+			if debug: print(file_name)
 		
-		if not path.exists(file_name):
-			print('downloading: ', file_name)
+			if not os.path.exists(file_name):
+				pass
+				#if debug: print('downloading: ', file_name)
 
-		# r = requests.get(url)
-		# open(file_name, 'wb').write(r.content)
+			# r = requests.get(url)
+			# open(file_name, 'wb').write(r.content)
 		
+
+if __name__ == "__main__":
+	PARSER = argparse.ArgumentParser(description='Unity LTS check and download updates')
+	PARSER.add_argument('-d', '--debug', help='debugging', action='store_true', default=False)
+	ARGS = PARSER.parse_args()
+
+	check_state(ARGS.debug)
