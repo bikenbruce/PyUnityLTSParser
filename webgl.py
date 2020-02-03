f9a014bf58c7f9d0a324188f0c4b47eb6b1d33a4
Bruce Gardner <bruce.gardner@sjsu.edu>
2020-02-03 13:23:50 -0800

    added check test scripts

---
+++ webgl.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import xml.etree.ElementTree as ET
+import os.path
+
+xml_file = "/Applications/Unity/PlaybackEngines/WebGLSupport/ivy.xml"
+current_version = '<VERSION>'
+
+if os.path.exists(xml_file):
+   tree = ET.parse(xml_file)
+   root = tree.getroot()
+
+   info = root.findall('info')
+
+   version = info[0].get('{http://ant.apache.org/ivy/extra}unityVersion')
+
+   if version != current_version:
+      # version does not match
+      exit(0)
+   else:
+      # version is okay
+      exit(1)
+
+else:
+   # software not installed
+   exit(0)
+
+# https://github.com/munki/munki/wiki/How-Munki-Decides-What-Needs-To-Be-Installed
+# exit code status of 0 means not installed, for install check script
+
